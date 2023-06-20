5c34cd268af0c802d39f3a92eddf80a9d17b80d4
Potter <potteramylynn@gmail.com>
2023-06-20 09:26:48 -0500

    commented out footer sections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 const Footer = () => {
   return(
     <div className= "App-footer">
-        <div>
+        {/* <div>
           <h4>Email</h4>
           <a href="mailto:admin@example.com">admin@example.com</a>
         </div>
         <div>
         <h4>Phone</h4>
         <a href="tel:9999999999">999-999-9999</a>
-        </div>
+        </div> */}
         <div>
         <h4>LinkedIn</h4>
         <a href="https://www.linkedin.com/in/amy-lynn-145aa527a/">Amy Potter</a>
