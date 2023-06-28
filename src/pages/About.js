e5fcc6bf8b55db7227e2a16f15e5a24b92df2de7
Potter <potteramylynn@gmail.com>
2023-06-27 19:05:11 -0500

    updated about text

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,8 +8,7 @@ export default function About() {
         <div className="About-text">
           <h1>About</h1>
           <p>
-            I am a full stack developer finishing boot camp with the University
-            of Minnesota this june. So far, I have worked with HTML, CSS, Javascript, NodeJS, MySQL2, MongoDB, ExpressJS, and Reactjs. I believe learning is a life-long endeavor and look forward to increasing my skill with these technologies and learning new ones as well.
+            I am a full stack developer who graduated with a certificate from the University of Minnesota June, 2023. So far, I have worked with HTML, CSS, Javascript, NodeJS, MySQL2, MongoDB, ExpressJS, and Reactjs. I have been a freelance artist since 2015, working in many mediums and enjoy adding custom logos designs to my projects as a developer. I believe learning is a life-long endeavor and look forward to increasing my skill with my current technologies and learning new ones as well.
           </p>
         </div>
         <div className="About-image">
